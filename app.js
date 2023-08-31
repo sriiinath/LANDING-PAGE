1092cd08c95daf43785e68dc8749290052701706
sriiinath <138140478+sriiinath@users.noreply.github.com>
2023-08-31 16:06:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// MOBILE NAVIGATION MENU
+const menuButton = document.querySelector('.menu');
+const navList = document.querySelector('.nav__list');
+
+menuButton.addEventListener('click', function () {
+    navList.classList.toggle('active');
+    this.classList.toggle('change-color');
+});
+
+// TESTIMONIAL SLIDER
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const slides = document.querySelectorAll('.testimonial__box--col');
+
+let index = 0;
+display(index); 
+function display(index) {
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[index].style.display = 'block';
+}
+
+function prevSlide() {
+    index--;
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+    display(index);
+}
+
+function nextSlide() {
+    index++;
+    if (index > slides.length - 1) {
+        index = 0;
+    }
+    display(index);
+}
+
+prev.addEventListener('click', prevSlide);
+next.addEventListener('click', nextSlide);
